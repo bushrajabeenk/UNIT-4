2eebceffc4c5e181a911951a8068969424596e91
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-06-08 16:44:35 +0530

    c

--- S4/day2/src/pages/Todo.jsx
+++ S4/day2/src/pages/Todo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo } from './../store/todo/todo.actions';
+import { addTodo } from "./../store/todo/todo.actions";
 
 const Todo = () => {
   const ref = useRef();
