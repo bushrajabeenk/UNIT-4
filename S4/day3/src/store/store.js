a9e920f930f337e3f599014b0a9f187835453a36
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-06-13 03:15:21 +0530

    chan

--- S4/day3/src/store/store.js
+++ S4/day3/src/store/store.js
@@ -9,6 +9,7 @@ import thunk from "redux-thunk";
 import { counterReducer } from "./counter/counter.reducer";
 import { todoReducer } from "./todo/todo.reducer";
 
+// this line checks if we have installed redux devtools or not
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
