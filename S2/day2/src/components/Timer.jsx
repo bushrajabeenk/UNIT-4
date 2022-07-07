016b83d6e7faf014e5244a456ff9a740e99b58e3
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-08 01:09:01 +0530

    changes

--- S2/day2/src/components/Timer.jsx
+++ S2/day2/src/components/Timer.jsx
@@ -12,10 +12,21 @@ const Timer = () => {
       }
     }, 1000);
 
+
+    //  these are called cleanup functions
+    // if the component is unmounted before the setInterval
+    // time runs out. For example in the above case, it is given 1000
+    // if the component is unmounted, ie if the component App
+    // is removed from the UI before the completion of 1000s
+    // ie if we remove App itself (which in turn contains Timer.jsx as component)
+    // then we don't want the timer to keep running, because the App
+    // itself is removed from UI, we don't want the timer to keep running
+    // so to stop the timer from running in such cases
+    // we use cleanup functiom
     return () => {
-        // call an api to inform that user has logged out
-        clearInterval(id);
-    }
+      // call an api to inform that user has logged out
+      clearInterval(id);
+    };
     // component is unmounting
   }, []);
 
--- S2/day3/src/App.js
+++ S2/day3/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import Form from './components/Form';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
